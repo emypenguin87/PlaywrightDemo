57fe41385cd7b2b8c3459a63656d68d74beeb069
emilyparker-sh <emily.parker@simplyhealth.co.uk>
2024-08-20 11:44:38 +0100

    Basic installation complete

---
+++ tests/example.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('I can find information about Charmander', async ({ request }) => {
+  const response = await request.post(`http://pokeapi.co/api/v2/pokemon/ditto`, {
+    headers: {
+      'Content-Type': 'application/json',
+  }})
+
+   console.log(await response.json())
+  expect(response.status()).toBe(200)
+  
+});
+
+
+test('I can log into to SwagLabs', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Swag Labs/);
+
+  await page.locator('[data-test="username"]').fill("standard_user");
+  await page.locator('[data-test="password"]').fill("secret_sauce");
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="shopping-cart-link"]')).toBeVisible();
+
+
+
+});
