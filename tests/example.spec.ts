6d574925959d940e7f31a72e23cc5fb218f5ba3c
emilyparker-sh <emily.parker@simplyhealth.co.uk>
2024-08-20 11:55:53 +0100

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,29 +17,5 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test('I can find information about Charmander', async ({ request }) => {
-  const response = await request.post(`http://pokeapi.co/api/v2/pokemon/ditto`, {
-    headers: {
-      'Content-Type': 'application/json',
-  }})
-
-   console.log(await response.json())
-  expect(response.status()).toBe(200)
-  
-});
 
 
-test('I can log into to SwagLabs', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Swag Labs/);
-
-  await page.locator('[data-test="username"]').fill("standard_user");
-  await page.locator('[data-test="password"]').fill("secret_sauce");
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('[data-test="shopping-cart-link"]')).toBeVisible();
-
-
-
-});
