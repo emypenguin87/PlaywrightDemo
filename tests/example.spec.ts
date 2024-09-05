3a92ee6b85030077864f63f704d23aa891468404
emilyparker-sh <emily.parker@simplyhealth.co.uk>
2024-09-05 16:44:40 +0100

    Example test added and updated pdf instructions

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,5 +17,30 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
+test('I can log into to Applitools demo site', async ({ page }) => {
+  await page.goto('https://demo.applitools.com/');
+
+  await page.locator('[src="img/logo-big.png"]').isVisible();
+
+  //HINT Here we are using the "id" attribute of an element to locate it, sometimes you may need to use other attributes eg line 23 uses src
+  await page.locator('[id="username"]').fill("test");
+  await page.locator('[id="password"]').fill("test");
+  await page.locator('[id="log-in"]').click();
+
+  await expect(page.locator('[id="time"]')).toBeVisible();
+
+
+});
+
+//Your test goes here:
+
+
+
+
+
+
+
+
+
 
 
