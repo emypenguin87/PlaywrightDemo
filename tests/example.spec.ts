a539571c1a7bdacf933a66dcac8d333200380ab0
SimplyhealthAutomation <simplyhealth.automation@gmail.com>
2024-09-11 16:06:34 +0100

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+/*test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -31,9 +31,45 @@ test('I can log into to Applitools demo site', async ({ page }) => {
 
 
 });
+*/
 
 //Your test goes here:
 
+
+test('has title', async ({ page }) => {
+  await page.goto('https://saucedemo.com');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Swag Labs/);
+});
+
+/*
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://saucedemo.com');
+
+  // Click the get started link.
+  /*await page.getByRole('link', { name: 'Sauce Labs Backpack' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+*/
+
+test('I can log into to Applitools demo site', async ({ page }) => {
+  await page.goto('https://saucedemo.com');
+
+ /* await page.locator('[src="/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg"]').isVisible();*/
+
+  //HINT Here we are using the "id" attribute of an element to locate it, sometimes you may need to use other attributes eg line 23 uses src
+  await page.locator('[id="user-name"]').fill("standard_user");
+  await page.locator('[id="password"]').fill("secret_sauce");
+  await page.locator('[id="login-button"]').click();
+
+  await expect(page.locator('[id="shopping_cart_container"]')).toBeVisible();
+
+
+});
 
 
 
